b4ee692165c6109dca268086f1cb94d3d3f728d2
helges1 <sebhelg@gmail.com>
2021-09-20 21:18:15 +0200

    fiksa på ting

--- Uke 5/uke_05_oppg_10.py
+++ Uke 5/uke_05_oppg_10.py
@@ -3,8 +3,8 @@ from math import sqrt
 
 def side_length(x1, y1, x2, y2):
     """Returns the length of the side between the points (x1,y1) and (x2,y2)"""
-    length = sqrt((x2-x1)**2 + (y2-y1)**2)
-    return length
+    leng = sqrt((x2-x1)**2 + (y2-y1)**2)
+    return leng
 
 
 def mid(x1_or_y1, x2_or_y2, x3_or_y3, a, b, c):
@@ -13,23 +13,16 @@ def mid(x1_or_y1, x2_or_y2, x3_or_y3, a, b, c):
     coordinates x1_or_y1, x2_or_y2, x3_or_y3
     and sidelengths a (opposite (x1,y1)), b (opposite (x2,y2)), c (opposite (x3,y3))
     """
-    x1_or_y1 = x1
-    x2_or_y2 = x2
-    x3_or_y3 = x3
-    midpointx = ((x1_or_y1 * a) + (x2_or_y2 * b) + (x3_or_y3 * c)) / (a + b + c)
-    x1_or_y1 = y1
-    x2_or_y2 = y2
-    x3_or_y3 = y3
-    midpointy = ((x1_or_y1 * a) + (x2_or_y2 * b) + (x3_or_y3 * c)) / (a + b + c)
-    return(midpointx, midpointy)
+    xy = (x1_or_y1 * a + x2_or_y2 * b + x3_or_y3 * c)/(a + b + c)
+    return xy
+
 
 def incircle_radius(a, b, c):
     """Returns the radius of the incircle of a triangle with sidelengths a, b and c"""
-    s = (a + b + c) / 2
-    radi = sqrt(((s-a)*(s-b)*(s-b))/2)
-    return radi
-
-
+    # din kode her
+    s = float((a + b + c)/2)
+    rad = float(sqrt((s - a)*(s - b)*(s - c)/(s)))
+    return rad
 
 def find_incircle(x1, y1, x2, y2, x3, y3):
     """
@@ -37,21 +30,20 @@ def find_incircle(x1, y1, x2, y2, x3, y3):
     of a triangle with corners (x1,y1), (x2,y2), (x3,y3)
     """
 
-
     print(f"Your triangle is ({x1}, {y1}), ({x2}, {y2}), ({x3}, {y3}).\n")
 
     print("First we calculate the sidelengths.")
-    side1 = 0  # side opposite (x1,y1)
-    side2 = 0  # side opposite (x2,y2)
-    side3 = 0  # side opposite (x3,y3)
+    side1 = side_length(x2, y2, x3, y3) # side opposite (x1,y1)
+    side2 = side_length(x1, y1, x3, y3) # side opposite (x2,y2)
+    side3 = side_length(x1, y1, x2, y2)  # side opposite (x3,y3)
 
     print("Then we find the center.")
-    center_x = 0
-    center_y = 0
+    center_x = mid(x1, x2, x3, side1, side2, side3)
+    center_y =  mid(y1, y2, y3, side1, side2, side3)
     print(f"The center is {center_x, center_y}.\n")
 
     print("Finally we calculate the radius of the incircle.")
-    radius = radi
+    radius = incircle_radius(side1, side2, side3)
     print(f"The incircle has radius {radius}.")
 
 
