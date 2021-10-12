a518556f276d5373598ff35a804f8c559d028731
helges1 <sebhelg@gmail.com>
2021-10-12 11:06:55 +0200

    fikk til nummer 4

--- Uke_8/test.py
+++ Uke_8/test.py
@@ -1,5 +1,8 @@
-i = 1
-while i < 6:
-  print(i)
-  i += 1 * 2
-  
+file = open(file, "a") 
+table_data = [
+    ['a', 'b', 'c'],
+    ['aaaaaaaaaa', 'b', 'c'], 
+    ['a', 'bbbbbbbbbb', 'c']
+]
+for row in table_data:
+    file.write("{: >20} {: >20} {: >20}".format(*row))
--- Uke_8/uke_08_oppg_4.py
+++ Uke_8/uke_08_oppg_4.py
@@ -1,5 +1,5 @@
-a = int(input("Faktor A: "))
-b = int(input("Faktor B: "))
+a = int(input("Factor A: "))
+b = int(input("Factor B: "))
 
 størst = max(a,b)
 minst = min(a,b)
@@ -12,7 +12,7 @@ max_count1 = 0
 tall_a = 1
 a_tall = []
 
-while tall_a < minst :
+while tall_a <= minst :
     a_tall.append(tall_a)
 
     if tall_a == 1:
@@ -20,7 +20,7 @@ while tall_a < minst :
         count += 1
 
     else:
-        tall_a = tall_a * 2
+        tall_a = tall_a *2
         count += 1
     
 b_tall = []
@@ -31,20 +31,19 @@ while count > 0:
     max_count += 1
     max_count1 += 1
 
-
 def print_x(minst):
-    coins = a_tall
+    mins = a_tall
     x = []
 
-    for c in coins[::-1]:
+    for c in mins[::-1]:
         
         while minst >= c:
             
             x.append("X")
             minst -= c
             break
+
         else:
-            
             x.append(" ")   
             
     return x[::-1]
@@ -52,12 +51,36 @@ def print_x(minst):
 X = print_x(minst)
 
 while max_count > 0:
-    print(f"{X[count]}    {a_tall[count]}     {b_tall[count]}")
+    print(f"{X[count]} {a_tall[count]: >4} {b_tall[count]: >4}")
     max_count -= 1
     count += 1
 
 print("=========================")
 
+plasser_med_x = [i for i, e in enumerate(X) if e == "X"]
 
+len_x = len(plasser_med_x)
 
+teller = 0
+a_tall_liste =[]
 
+while teller < len_x:
+    a_tall_liste.append(a_tall[plasser_med_x[teller]])
+    teller += 1
+
+teller = 0
+b_tall_liste =[]
+
+while teller < len_x:
+    b_tall_liste.append(b_tall[plasser_med_x[teller]])
+    teller += 1
+
+a_list_str = list(map(str,a_tall_liste))
+print (' + '.join(a_list_str), "=", sum(a_tall_liste))
+
+b_list_str = list(map(str,b_tall_liste))
+print (' + '.join(b_list_str), "=", sum(b_tall_liste))
+
+print("=========================")
+
+print(f"{a} * {b} = {a * b}")
