194475ffdc25ab1f8384ed77155aede0fa70909b
helges1 <sebhelg@gmail.com>
2021-10-28 18:48:21 +0200

    .

--- Uke_10/uke_10_oppg_2.py
+++
@@ -1,7 +0,0 @@
-def open_file(filename):
-
-    with open(filename, encoding="utf8") as f:
-
-        return "\n".join([f">>>{line.strip()}<<<" for line in f.readlines()])  
-
-# print(open_file("askeladden.txt")) 
--- Uke_10/uke_10_oppg_4.py
+++
@@ -1,8 +0,0 @@
-def first_letter_last_word(filename):
-    with open(filename, encoding="utf8") as f:
-        x = f.readlines()
-        x = x[0]
-        return(x[::-1])
-
-     
-print(first_letter_last_word("askeladden.txt",))
